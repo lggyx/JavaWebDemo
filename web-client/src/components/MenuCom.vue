632a398e5378ed8627be1720175ea15a1607a5d5
lggyx <19546328912@163.com>
2025-08-31 20:37:28 +0800

    添加使用el-button方式加v-for进行菜单导航

--- web-client/src/components/MenuCom.vue
+++ web-client/src/components/MenuCom.vue
@@ -1,16 +1,22 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-
-export default defineComponent({
-  name: "MenuCom"
-})
+<script lang="ts" setup>
+import {ElButton} from 'element-plus'
+import router from '../router'
+const MenuData = [
+  { name: '首页', path: '/' },
+  { name: '注册', path: '/register' },
+  { name: '登录', path: '/login' }
+]
+const handleRouter = (path: string) => {
+  router.push(path)
+  }
 </script>
 
 <template>
   <div>
-    <router-link to="/">Home</router-link>|
+    <el-button type="primary" size="small" v-for="item in MenuData" @click="handleRouter(item.path)">{{ item.name }}</el-button>
+    <!-- <router-link to="/">Home</router-link>|
     <router-link to="/register">Register</router-link>|
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> -->
   </div>
 </template>
 
