426abf9b3e9dbece455dc31b97ddaabde4341a96
lggyx <19546328912@163.com>
2025-08-31 10:14:34 +0800

    添加DemoSelectById测试代码

--- src/test/java/com/lggyx/JavaWebDemoApplicationTests.java
+++ src/test/java/com/lggyx/JavaWebDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.lggyx;
 
+import com.lggyx.controller.DemoController;
+import com.lggyx.pojo.Result;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JavaWebDemoApplicationTests {
 
+    @Autowired
+    private DemoController demoController;
     @Test
-    void contextLoads() {
+    public void demoSelectById(){
+        Result result = demoController.selectById("1");
+        System.out.println(result);
     }
 
 }
